02e5f6c697153c0a4862f51b244e88bd24bab428
gisela <123730901+giiselaa@users.noreply.github.com>
2023-09-28 17:26:12 +0200

    cambio en ej.1 y ejemplo5

--- Ejemplo_1.cpp
+++ Ejemplo_1.cpp
@@ -1,18 +1,18 @@
-#include <iostream>>
+#include <iostream>
 using namespace std;
 
-void Ejemplo_1(int a, int& b);
+void ejemplo(int a, int& b);
 
 int main(int argc, char** argv)
 {
     int x = 2, y = 4;
     cout << "1. x = " << x << "y = " << y << endl;
-    Ejemplo_1(x, y);
+    ejemplo(x, y);
     cout << "2. x = " << x << "y = " << y << endl;
     return 0;
 }
 
-void Ejemplo_1(int a, int& b)
+void ejemplo(int a, int& b)
 {
     a = 5;
     b = 8;
